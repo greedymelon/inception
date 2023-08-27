562bd7c574cdd52450462d120c100ef80341fd56
greedymelon <74475072+greedymelon@users.noreply.github.com>
2023-08-27 12:43:36 +0200

    Update wordpress.sh

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#if not wordpre in the bind mount
-if ! [[ "$(ls -A /var/www/html/)" ]]; then
+#if not wordpress in the bind mount
+if ! [[ -f "/var/www/html/wp-config.php" ]]; then
 #download, configure the wordpress in the bind mount
 wp core download --allow-root --locale=en_GB
 wp config create --allow-root --dbname=$DATAB_NAME --dbuser=$USERDB_NAME --dbpass=$USERDB_PASS --dbhost="mariadb:3306" --skip-check
-# sleep to wait for the database and avoid installing extra tool
+# sleep to wait for the database and avoiding installing extra tool
 sleep 3
 wp core install --allow-root --url=$DOMAIN_NAME --title=Inception --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL
 wp user create --allow-root $NOPOW_NAME $NOPOW_EMAIL --user_pass=$NOPOW_PASS
@@ -15,7 +15,7 @@ wp post create --allow-root --post_type=page --post_title='Leave a comment' --po
 echo "New website createrd"
 fi
 
-#give right to php
+#gives right to php
 chown -R www-data:www-data /var/www/html/
 chmod -R 775 /var/www/html/
 
