fbb5aa5dfdda2a0f3a8133b26a44b7851bb36c1b
greedymelon <74475072+greedymelon@users.noreply.github.com>
2023-08-30 11:41:16 +0200

    Update database_init.sh

--- srcs/requirements/mariadb/tools/database_init.sh
+++ srcs/requirements/mariadb/tools/database_init.sh
@@ -8,21 +8,25 @@ if ! [[ "$(ls -A /var/lib/mysql)" ]]; then
     mariadb-install-db --datadir=/var/lib/mysql 
     echo "MYSQL initialized"
     service mariadb start
+    sleep 2
      # faking myysql_secure_installation.sh
     mariadb -uroot --password=$MYSQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
     mariadb -uroot --password=$MYSQL_ROOT_PASSWORD -e "DELETE FROM mysql.user WHERE User='';"
     mariadb -uroot --password=$MYSQL_ROOT_PASSWORD -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
     mariadb -uroot --password=$MYSQL_ROOT_PASSWORD -e "DROP DATABASE test;"
     mariadb -uroot --password=$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+    mysqladmin -uroot --password=$MYSQL_ROOT_PASSWORD shutdown
    
 fi 
 if ! [[ -d  "/var/lib/mysql/$DATAB_NAME" ]]; then
     #create users and wordpress database
-    mariadb -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+    service mariadb start
+    sleep 2
+    mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
     mariadb -uroot --password=$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $DATAB_NAME;"
     mariadb -uroot --password=$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$USERDB_NAME'@'%' IDENTIFIED BY '$USERDB_PASS';"
     mariadb -uroot --password=$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON $DATAB_NAME.* TO '$USERDB_NAME'@'%';"
-    mariadb -uroot --password=$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON * . * TO 'root'@'localhost' IDENTIFIED BY '$ROOT_PASS';"
+    mariadb -uroot --password=$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON * . * TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORDS';"
     mariadb -uroot --password=$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$NOPOW_NAME'@'%' IDENTIFIED BY '$NOPOW_PASS';"
     mariadb -uroot --password=$MYSQL_ROOT_PASSWORD -e "GRANT ALTER PRIVILEGES ON $DATAB_NAME.* TO '$NOPOW_NAME'@'%';"
     mariadb -uroot --password=$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
